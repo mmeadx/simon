6dddebde5747442ecbfc2998759dd50f76043d2b
Matt Mead <mattmead@matts-mbp.lan>
2021-01-09 14:41:29 -0600

    adding initial files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# simon
-Simon Says Web App
+# Simon Says Game
+
+_This is a web app built using JavaScript and JQuery based on a project from [The Complete 2021 Web Development Bootcamp from Udemy](https://www.udemy.com/share/101qYwAEEYeVpVTXgB/) modified to have a 'replay' button and slightly modified design_
+
+[PLAY HERE]()
---
+++ game.js
@@ -0,0 +1,148 @@
+// Level Count
+var level = 0;
+
+// Game Started - T of F
+var gameStatus = false;
+
+// Button Color Array
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+// Array to hold Computer's pattern
+var gamePattern = [];
+
+// Array to hold User's pattern
+var userPattern = [];
+
+// Keypress to start game
+$(document).keypress(function () {
+  if (!gameStatus) {
+
+    // Change level of game
+    $("#level-title").text("Level " + level);
+
+    // Call next sequence
+    nextSequence();
+
+    // Change game status to start
+    gameStatus = true;
+  }
+});
+
+$("#replay").click(() => {
+
+  console.log("Replay clicked");
+  replay();
+
+})
+// Click Handler for button clicks
+$(".btn").click(function () {
+
+  var userChosenColor = $(this).attr("id");
+  userPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkPattern(userPattern.length - 1);
+
+});
+
+// For the next button in the computer's sequence
+function nextSequence() {
+
+  userPattern = [];
+
+  // Up the level count by 1
+  level++;
+
+  // Change Level 
+  $("#level-title").text("Level " + level);
+
+  // Chooses random number
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  // Uses random number 0-3 to choose golor in "buttonColors" array
+  var randomChosenColor = buttonColors[randomNumber];
+
+  // Pushes chosen color to the gamePattern array
+  gamePattern.push(randomChosenColor);
+
+  // Animates button chosen
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  // Plays sound of button using random chosen color
+  playSound(randomChosenColor);
+
+
+}
+
+// Play Sound Function
+function playSound(colorSound) {
+
+  // Concatenates path to find sound using color name
+  var audio = new Audio("sounds/" + colorSound + ".mp3");
+  audio.play();
+}
+
+// Animate Button Press Function
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");;
+  }, 100);
+}
+
+// Function to check User input against Computer's
+function checkPattern(currentLevel) {
+
+  // Last clicked button
+  var userPick = userPattern[currentLevel];
+  var compPick = gamePattern[currentLevel];
+
+  // Check against computer
+  if (userPick == compPick) {
+    // console.log("Correct");
+    if (userPattern.length === gamePattern.length) {
+
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else {
+
+    // Plays 'wrong' sound effect
+    playSound("wrong");
+
+    // Background flashes red
+    $("body").addClass("game-over");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over")
+    }, 300);
+
+    // h1 sets to "Game over"
+    $("h1").text("GAME OVER - Press any key to replay");
+
+    startOver();
+  }
+
+}
+
+// Function to reset game
+
+function startOver() {
+  gameStatus = false;
+  level = 0;
+  gamePattern = [];
+}
+
+// Function to play back computer pattern
+function replay() {
+
+  var color = gamePattern[gamePattern.length - 1];
+  // console.log(color);
+  playSound(color);
+  animatePress(color);
+
+}
